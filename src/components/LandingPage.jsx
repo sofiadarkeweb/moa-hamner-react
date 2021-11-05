0cf5b73356d4335fedc8fcee6f2e089c4a30881c
SofiaDarke <sofiadarke@earthpeople.se>
2021-11-05 16:06:53 +0100

    Add landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Aframe from "./Aframe";
+import { Link } from "react-router-dom";
+import Rose from "../media/rose.jpg";
+
+const LandingPage = () => {
+	// function doSomething() {
+	// 	console.log("hej");
+	// }
+	return (
+		<div className="landing-page">
+			<h2 className="landing-sub-title">ljuddesign</h2>
+			<header className="landing-header">
+				<Link className="title" to="/about">
+					Moa <br />
+					Hamner
+				</Link>
+			</header>
+
+			{/* <img className="landing-picture" src={Eye} alt="My eye" /> */}
+			<img className="landing-picture" src={Rose} alt="rose" />
+
+			<div className="landing-square">
+				<div className="aframe-scene">
+					<Aframe />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LandingPage;

b0ffd4d16a5bcaeefb49193f5ec67b7fbc537b12
SofiaDarke <sofiadarke@earthpeople.se>
2021-11-05 16:08:03 +0100

    Add overall site component

---
+++ src/components/Site.js
@@ -0,0 +1,32 @@
+// import Projects from "./Projects";
+import About from "./About";
+import LandingPage from "./LandingPage";
+import Work from "./Work";
+import Footer from "./Footer";
+
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+const Site = () => {
+	return (
+		<div>
+			<Router basename="/">
+				<div>
+					<Switch>
+						<Route exact path="/">
+							<LandingPage />
+						</Route>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/work">
+							<Work />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+			<Footer />
+		</div>
+	);
+};
+
+export default Site;

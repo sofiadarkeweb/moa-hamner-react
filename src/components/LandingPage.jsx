488270b53ae8289cb3fbcfe7eb428772c6444f00
SofiaDarke <sofiadarke@earthpeople.se>
2021-11-12 13:13:51 +0100

    Change react router dom

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Aframe from "./Aframe";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Rose from "../media/rose.jpg";
 
 const LandingPage = () => {
@@ -11,11 +11,11 @@ const LandingPage = () => {
 		<div className="landing-page">
 			<h2 className="landing-sub-title">ljuddesign</h2>
 			<header className="landing-header">
-				<h1 className="title">Moa Hamner</h1>
-				{/* <Link className="title" to="/about">
+				{/* <h1 className="title">Moa Hamner</h1> */}
+				<Link className="title" to="/about">
 					Moa <br />
 					Hamner
-				</Link> */}
+				</Link>
 			</header>
 
 			{/* <img className="landing-picture" src={Eye} alt="My eye" /> */}
--- src/components/Site.js
+++ src/components/Site.js
@@ -1,32 +1,22 @@
 // import Projects from "./Projects";
-// import About from "./About";
+import About from "./About";
 import LandingPage from "./LandingPage";
-// import Work from "./Work";
+import Work from "./Work";
 import Footer from "./Footer";
 
-// import { HashRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 //TODO solve react router
 
 const Site = () => {
 	return (
 		<div>
-			<LandingPage />
-			{/* <Router basename="/">
-				<div>
-					<Switch>
-						<Route exact path="/">
-							<LandingPage />
-						</Route>
-						<Route path="/about">
-							<About />
-						</Route>
-						<Route path="/work">
-							<Work />
-						</Route>
-					</Switch>
-				</div>
-			</Router> */}
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/work" element={<Work />} />
+			</Routes>
 			<Footer />
 		</div>
 	);

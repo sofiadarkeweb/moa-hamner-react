015bba529276e863e902cd709edacffbc7727238
SofiaDarke <sofiadarke@earthpeople.se>
2022-01-06 17:40:25 +0100

    Footer removed small sizes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,19 +6,25 @@ function Project(props) {
 	const [hiddenImage, sethiddenImage] = useState(false);
 
 	return (
-		<div
-			className="cv-text"
-			onMouseEnter={() => sethiddenImage(true)}
-			onMouseLeave={() => sethiddenImage(false)}
-		>
-			{hiddenImage && (
-				<img className="movie-img" alt="Still from movie" src={props.img}></img>
-			)}
-			<p className="project-title">{props.title}</p>
-			<p className="project-sub">
-				{props.info} {props.year}
-			</p>
-		</div>
+		<>
+			<div
+				className="cv-text"
+				onMouseEnter={() => sethiddenImage(true)}
+				onMouseLeave={() => sethiddenImage(false)}
+			>
+				{hiddenImage && (
+					<img
+						className="movie-img"
+						alt="Still from movie"
+						src={props.img}
+					></img>
+				)}
+				<p className="project-title">{props.title}</p>
+				<p className="project-sub">
+					{props.info} {props.year}
+				</p>
+			</div>
+		</>
 	);
 }
 
